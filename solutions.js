7415eb9aa82d82bd84140053adcc82db58aa1b76
Mao Suarez <maosuarezbarrer@gmail.com>
2024-08-14 14:54:44 -0500

    Prueba 2

--- solutions.js
+++ solutions.js
@@ -1,10 +1,16 @@
 // Sum of Two Numbers
 function sum(a, b) {
+  if (!(Number.isInteger(a) && Number.isInteger(b))) {
+    return "Los datos de entrada no son adecuados";
+  }
   return Number(a) + Number(b);
 }
 
 // Factorial of a Number
 function factorial(n) {
+  if (!Number.isInteger(n)) {
+    return "Los datos de entrada no son adecuados";
+  }
   function procedimiento(result, paso) {
     if (paso === 0) {
       return result;
@@ -21,7 +27,10 @@ function factorial(n) {
 
 // Find the Largest Number
 function findLargest(arr) {
-  let max = 0;
+  let max = arr.pop();
+  if (!Number.isInteger(max)) {
+    return "Los datos de entrada no son adecuados";
+  }
   while (arr) {
     if (max < arr.pop()) {
       max = arr.pop();
@@ -32,6 +41,9 @@ function findLargest(arr) {
 
 // Count Vowels in a String
 function countVowels(str) {
+  if (Number.isInteger(str)) {
+    return "Los datos de entrada no son adecuados";
+  }
   let vowels = ["a", "e", "i", "o", "u"];
   let count = 0;
   for (let i = 0; i < str.length; i++) {
@@ -44,6 +56,9 @@ function countVowels(str) {
 
 // Check if a Number is Prime
 function isPrime(n) {
+  if (!Number.isInteger(n)) {
+    return "Los datos de entrada no son adecuados";
+  }
   if (n <= 1) {
     return false;
   }
