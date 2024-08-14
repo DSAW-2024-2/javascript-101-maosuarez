9c5f8f5844ae0cc9b05799dad465b699a9fbf2a0
Mao Suarez <maosuarezbarrer@gmail.com>
2024-08-14 14:42:43 -0500

    Prueba 1

--- solutions.js
+++ solutions.js
@@ -1,26 +1,62 @@
 // Sum of Two Numbers
 function sum(a, b) {
-  // Add your code here
+  return Number(a) + Number(b);
 }
 
 // Factorial of a Number
 function factorial(n) {
-  // Add your code here
+  function procedimiento(result, paso) {
+    if (paso === 0) {
+      return result;
+    } else {
+      return procedimiento(result * paso, paso - 1);
+    }
+  }
+
+  if (n === 0) {
+    return 1;
+  }
+  return procedimiento(1, n);
 }
 
 // Find the Largest Number
 function findLargest(arr) {
-  // Add your code here
+  let max = 0;
+  while (arr) {
+    if (max < arr.pop()) {
+      max = arr.pop();
+    }
+  }
+  return max;
 }
 
 // Count Vowels in a String
 function countVowels(str) {
-  // Add your code here
+  let vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i].toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
 }
 
 // Check if a Number is Prime
 function isPrime(n) {
-  // Add your code here
+  if (n <= 1) {
+    return false;
+  }
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      count++;
+    }
+  }
+  if (count == 2) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = {
