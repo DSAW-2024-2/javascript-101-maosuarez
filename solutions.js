8774d68a62631a1a76a9430c8955374b85704cda
Mao Suarez <maosuarezbarrer@gmail.com>
2024-08-17 12:55:04 -0500

    Prueba final

--- solutions.js
+++ solutions.js
@@ -1,16 +1,20 @@
-// Sum of Two Numbers
+// npm run-script test
+
 function sum(a, b) {
-  if (!(Number.isInteger(a) && Number.isInteger(b))) {
-    return "Los datos de entrada no son adecuados";
+  if (
+    typeof a === "number" &&
+    !isNaN(a) &&
+    typeof b === "number" &&
+    !isNaN(b)
+  ) {
+    return a + b;
+  } else {
+    console.log("Los datos de entrada no son adecuados");
   }
-  return Number(a) + Number(b);
 }
 
 // Factorial of a Number
 function factorial(n) {
-  if (!Number.isInteger(n)) {
-    return "Los datos de entrada no son adecuados";
-  }
   function procedimiento(result, paso) {
     if (paso === 0) {
       return result;
@@ -19,59 +23,66 @@ function factorial(n) {
     }
   }
 
-  if (n === 0) {
-    return 1;
+  if (typeof n === "number" && !isNaN(n)) {
+    if (n === 0) {
+      return 1;
+    }
+    return procedimiento(1, n);
+  } else {
+    return "Los datos de entrada no son adecuados";
   }
-  return procedimiento(1, n);
 }
 
 // Find the Largest Number
 function findLargest(arr) {
-  let max = arr.pop();
-  if (!Number.isInteger(max)) {
-    return "Los datos de entrada no son adecuados";
-  }
-  while (arr) {
-    if (max < arr.pop()) {
-      max = arr.pop();
+  let max = arr[0];
+  if (typeof max === "number" && !isNaN(max)) {
+    for (let i = 1; i < arr.length; i++) {
+      if (max < arr[i]) {
+        max = arr[i];
+      }
     }
+    return max;
+  } else {
+    console.log("Los datos de entrada no son adecuados");
   }
-  return max;
 }
 
 // Count Vowels in a String
 function countVowels(str) {
-  if (Number.isInteger(str)) {
-    return "Los datos de entrada no son adecuados";
-  }
-  let vowels = ["a", "e", "i", "o", "u"];
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (vowels.includes(str[i].toLowerCase())) {
-      count++;
+  if (typeof str === "number" && !isNaN(str)) {
+    console.log("Los datos de entrada no son adecuados");
+  } else {
+    let vowels = ["a", "e", "i", "o", "u"];
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (vowels.includes(str[i].toLowerCase())) {
+        count++;
+      }
     }
+    return count;
   }
-  return count;
 }
 
 // Check if a Number is Prime
 function isPrime(n) {
-  if (!Number.isInteger(n)) {
-    return "Los datos de entrada no son adecuados";
-  }
-  if (n <= 1) {
-    return false;
-  }
-  let count = 0;
-  for (let i = 1; i <= n; i++) {
-    if (n % i === 0) {
-      count++;
+  if (typeof n === "number" && !isNaN(n)) {
+    if (n <= 1) {
+      return false;
     }
-  }
-  if (count == 2) {
+    let count = 0;
+    for (let i = 1; i <= n; i++) {
+      if (n % i === 0) {
+        count++;
+      }
+    }
+    if (count != 2) {
+      return false;
+    }
     return true;
+  } else {
+    console.log("Los datos de entrada no son adecuados");
   }
-  return false;
 }
 
 module.exports = {
